570a2f9e1828c7c1d0550e74df021333c2270225
Dmytro Kyrychuk <dmytro@kyrych.uk>
2025-06-12 10:01:08 +0300

    Work around missing kitty terminfo

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -1,11 +1,14 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{...}: {
   programs.kitty.enable = true;
   programs.kitty.theme = "Ayu Light";
 
   xsession.windowManager.i3.config.terminal = "kitty";
+
+  # Fix issues with missing terminfo when connecting to remote machines via SSH
+  programs.kitty.environment = {
+    TERM = "xterm-256color";
+  };
+  programs.ssh.extraConfig = ''
+    SetEnv TERM=xterm-256color
+  '';
 }
